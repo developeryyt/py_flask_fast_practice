d00872e7d16303a461c9f69f47e5b90031c28ea5
developeryyt <yeontaey92@gmail.com>
2025-09-04 18:43:25 +0900

    flask template test

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+import requests
 
 
 app = Flask(__name__)
@@ -26,6 +27,39 @@ def get_profile(username):
 def get_message(message_id):
     print(type(message_id), message_id)
     return "message id: %d " % message_id
+
+@app.route('/myinfo')
+def get_myinfo():
+    data = { 'name': 'patrick', "age": 30 }
+    return jsonify(data)
+
+@app.route('/login')
+def login():
+    username = request.args.get('user_name')
+    pw = request.args.get('pw')
+    email = request.args.get('email_address')
+
+    print(username, 'username', email, pw)
+    if username == 'patrick':
+        return_data = { 'auth' : "success" }
+    else:
+        return_data = { 'auth' : "fail" }
+    return jsonify(return_data)
+
+@app.route('/hello/<user>')
+def hello_user(user):
+    return render_template('variable.html', name1=user, name2='kim')
+
+@app.route('/loop')
+def loop_test():
+    info_list = ['list1', 'list2', 'list3']
+    return render_template('loop.html', values=info_list)
+
+
+@app.route("/google")
+def get_google():
+    response = requests.get("http://www.google.co.kr")
+    return response.text
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
